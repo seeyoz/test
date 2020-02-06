d25190643480749595bcad71cd796e13544a05c3
左撇峰子 <zpfz@seeyoz.cn>
2020-02-07 00:18:02 +0800

    no message
    Signed-off-by: 左撇峰子 <zpfz@seeyoz.cn>

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# test
+<div align=center><img src="https://s2.ax1x.com/2020/02/04/1DsmTO.png" height = "100" div align=center /></div>
+<h1 align="center">VuePress-cli</h1>
+<div align="center">
+
+A lightweight cli for VuePress project.
+
+![Build](https://img.shields.io/badge/build-passing-brightgreen?style=flat-square) ![Release](https://img.shields.io/badge/release-v1.0.0-blue?style=flat-square) ![Version](https://img.shields.io/badge/license-MIT-brightgreen?style=flat-square)
+
+</div>
+
+# Features
+- Devote one's energy to writing, instead of wasting time building the writing environment.
+- Optimize parts of styles.
+
+# Docs
+See here for more details : [https://zpfz.github.io/vuepress-cli/](https://zpfz.github.io/vuepress-cli/)
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  title: "VuePress-cli",
+  description: "A lightweight cli for VuePress project.",
+  dest: "./dist",
+  base: "{{ base }}",
+  head: [
+    ["link",{ rel: "icon",href: "/assets/logo.png" }]
+  ],
+  markdown: {
+    lineNumbers: {{ lineNumbers }} // close lineNumbers
+  },
+  themeConfig: {
+    smoothScroll: true,
+    nav: require("./nav"),
+    sidebar: require("./sidebar"),
+    sidebarDepth: 2,
+    lastUpdated: "Last Updated",
+    searchMaxSuggestoins: 10,
+    repo: "{{ repo }}",
+    editLinks: {{ editLinks }},
+    editLinkText: "Is something wrong or missing? Edit this page on github!"
+  },
+  module: {
+    {{ ruleLess }}
+    // rules: [
+    //   {
+    //     test: /\.less$/,
+    //     loader: "less-loader", // compiles Less to CSS
+    //   },
+    // ],
+  }
+};
